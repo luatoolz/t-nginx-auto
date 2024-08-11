9944210da05377853de4dbd241555137ad2bc86d
luatoolz <luatoolz@somahorse.com>
2024-08-11 07:36:04 +0300

    fixes + refactoring + simplify

--- lua/t/nginx/auto/method.lua
+++ lua/t/nginx/auto/method.lua
@@ -7,16 +7,16 @@ local is = t.is
 
 local api = {
   POST='PUT',
-  GET = function(o) return o[ngx.var.id] end,
-  HEAD = function(o) return o % ngx.var.id end,
-  DELETE = function(o) return o-ngx.var.id end,
-  PUT = function(o) return o+ngx.req.get_body_data() end,
+  GET=function(o) return o[ngx.var.id] end,
+  HEAD=function(o) return o % ngx.var.id end,
+  DELETE=function(o) return o - ngx.var.id end,
+  PUT=function(o) return o + ngx.req.get_body_data() end,
 }
 
 return function(o)
-	local method = api[ngx.var.request_method]
-	if type(method)=='string' then method=api[method] end
-	if not is.callable(method) then return ngx.exit(500) end
-  assert(type(o)=='table' and type(getmetatable(o))=='table', ('t.nginx.auto.method: invalid argument: await %s, got %s'):format('object', type(o)))
-	return method(o)
+  local method = api[ngx.var.request_method]
+  if type(method) == 'string' then method = api[method] end
+  if not is.callable(method) then return ngx.exit(500) end
+  assert(type(o) == 'table' and type(getmetatable(o)) == 'table', ('t.nginx.auto.method: invalid argument: await %s, got %s'):format('object', type(o)))
+  return method and method(o) or nil
 end
--- lua/t/nginx/auto/response.lua
+++ lua/t/nginx/auto/response.lua
@@ -8,42 +8,48 @@ local is = t.is
 local json = t.format.json
 
 local api = {
-	GET = function(r)
-    if type(r)=='nil' then return ngx.exit(404) end
-    if type(r)=='boolean' then return ngx.exit(r and 200 or 404) end
-    if type(r)=='number' then ngx.say(tostring(r)); return ngx.exit(200) end
-    if type(r)=='string' then if r~='' then ngx.say(r) end; return ngx.exit(200) end
-    if type(r)=='table' then
-      local inspect = require "inspect"
-      ngx.log(ngx.NOTICE, ' GET.r=' .. inspect(r))
---      r=json(r)
---      assert(type(r)=='table')
+  GET=function(r)
+    if type(r) == 'nil' then return ngx.exit(404) end
+    if type(r) == 'boolean' then return ngx.exit(r and 200 or 404) end
+    if type(r) == 'number' then
+      ngx.say(tostring(r));
+      return ngx.exit(200)
+    end
+    if type(r) == 'string' then
+      if r ~= '' then ngx.say(r) end
+      return ngx.exit(200)
+    end
+    if type(r) == 'table' then
       ngx.say(json.encode(r))
       return ngx.exit(200)
     end
     return ngx.exit(500)
   end,
-	HEAD = function(r)
-    if type(r)=='nil' then return ngx.exit(404) end
-    if type(r)=='table' then
+  HEAD=function(r)
+    if type(r) == 'nil' then return ngx.exit(404) end
+    if type(r) == 'table' then
       if is.bulk(r) then
-        r=tonumber(r) or #r
+        r = tonumber(r) or #r
       else
-        r=toboolean(r)
+        r = toboolean(r)
       end
     end
-    if type(r)=='boolean' then return ngx.exit(r and 200 or 404) end
-    if type(r)=='number' then ngx.header['X-Count']=r; return ngx.exit(r>0 and 200 or 404) end
-    if type(r)=='string' then return ngx.exit(#r>0 and 200 or 404) end
+    if type(r) == 'boolean' then return ngx.exit(r and 200 or 404) end
+    if type(r) == 'number' then
+      ngx.header['X-Count'] = r;
+      return ngx.exit(r > 0 and 200 or 404)
+    end
+    if type(r) == 'string' then return ngx.exit(#r > 0 and 200 or 404) end
     return ngx.exit(500)
   end,
-	DELETE = function(r) return ngx.exit(toboolean(r) and 200 or 404) end,
-  POST='DELETE', PUT='DELETE',
+  DELETE=function(r) return ngx.exit(toboolean(r) and 200 or 404) end,
+  POST='DELETE',
+  PUT='DELETE',
 }
 
 return function(r)
-	local method = api[ngx.var.request_method]
-	if type(method)=='string' then method=api[method] end
-	if not is.callable(method) then return ngx.exit(500) end
-	return method and method(r) or nil
+  local method = api[ngx.var.request_method]
+  if type(method) == 'string' then method = api[method] end
+  if not is.callable(method) then return ngx.exit(500) end
+  return method and method(r) or nil
 end
